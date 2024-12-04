05c52ae4b03748aec417733bd21279b80c910a49
pbabu01t <pbabu01t@knowledgenettest.onmicrosoft.com>
2024-12-04 08:01:07 +0530

    Classes - Part 2

--- playground/es6-classes-1.js
+++ playground/es6-classes-1.js
@@ -8,7 +8,7 @@ class Person {
         return `Hi, I am ${this.name}!`
     }
     getDescription() {
-        return `${this.name} is ${this.age} year(s) old`
+        return `${this.name} is ${this.age} year(s) old.`
     }
 }
 
@@ -20,17 +20,37 @@ class Student extends Person {
     hasBranch() {
         return !!this.branch
     }
+    getDescription() {
+        let description = super.getDescription()
+        if (this.hasBranch()) {
+            description = description + ` And, my branch is ${this.branch}`
+        }
+        return description
+    }
 }
 
-const me = new Student('naren', 30, "Computer Science")
+class Traveller extends Person {
+    constructor(name, age, homelocation) {
+        super(name, age)
+        this.homelocation = homelocation
+    }
+    hasHomeLocation() {
+        return !!this.homelocation
+    }
+    getDescription() {
+        let description = super.getDescription()
+        if (this.hasHomeLocation()) {
+            description += ` And my home location is ${this.homelocation}`
+        }
+    }
+}
 
-console.log(me)
-console.log(me.hasBranch())
-// console.log(me.getGreeting())
-// console.log(me.getDescription())
+const tobj = new Traveller("Naren", 30, "Bangalore")
+console.log(tobj)
+console.log(tobj.getDescription())
+console.log(tobj.hasHomeLocation())
 
-const other = new Student()
-
-console.log(other.hasBranch())
-// console.log(other.getGreeting())
-// console.log(other.getDescription())
+const tobjEmpty = new Traveller()
+console.log(tobj)
+console.log(tobj.getDescription())
+console.log(tobj.hasHomeLocation())
