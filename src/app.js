e4483ebd40bfcc082a69b985f8c00a5646632ed4
pbabu01t <pbabu01t@knowledgenettest.onmicrosoft.com>
2024-12-05 14:11:41 +0530

    Nesting Components

--- src/app.js
+++ src/app.js
@@ -1,16 +1,75 @@
-class Header extends React.Component {
-    render(){
-        return <p>Hey this is from Header Component</p>
+class IndecisionApp extends React.Component {
+    render() {
+        return <div>
+            <Header />
+            <Action />
+            <Options />
+            <AddOption />
+        </div>
     }
 }
 
-const jsx = (
-    <div>
-        <h1>Hello World!</h1>
-        <Header />
-        <Header />
-        <Header />
-    </div>
-)
 
-ReactDOM.render(jsx, document.getElementById('app'))
+class Header extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <p>Put your hands in the life of a computer.</p>
+            </div>
+        )
+    }
+}
+
+
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
+    }
+}
+
+
+// Options -> Options Component Here
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>Count: []</h3>
+                <Option />
+                <Option />
+                <Option />
+            </div>
+        )
+    }
+}
+
+class Option extends React.Component {
+    render() {
+        return (
+            <p>Option Text Here.</p>
+        )
+    }
+}
+
+
+// AddOption -> AddOption Component Here
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" name="option" />
+                    <button>Add Option</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'))
