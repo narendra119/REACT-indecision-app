27b5242f2e47c761154ddfdc7aa61d72318528b1
pbabu01t <pbabu01t@knowledgenettest.onmicrosoft.com>
2024-12-14 09:01:14 +0530

    Visiblity toggle in React Component Style

--- playground/build-it-visibility.js
+++ playground/build-it-visibility.js
@@ -1,24 +1,59 @@
+// Visibility Toggle in React Component Style
 
-let toggle = true;
-const changeToggle = () => {
-    toggle = !toggle
-    console.log("toggle", toggle)
-    render()
-};
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggleVisibility = this.toggleVisibility.bind(this)
+        this.state = {
+            toggle : false
+        }
+    }
 
-const app = document.getElementById("app")
+    toggleVisibility () {
+        console.log("Visibility Toggle!!!")
+        this.setState((prevState) => {
+            return {
+                toggle: !prevState.toggle
+            }
 
+        })
+    }
 
-const render = () => {
-    const template = (
-        <div>
-            <h1>Visibility Toggle</h1>
-            <button onClick={changeToggle} toggle={true}>{toggle ? 'Show Details' : 'Hide Details'}</button>
-            <p hidden={toggle}>Hey, this is the magic text that gets hidden and appear everytime you click the button above.</p>
-        </div>
-    )
-
-    ReactDOM.render(template, app)
+    render() {
+        return (
+            <div>
+                <h1>VisibilityToggle: {this.state.toggle.toString()}</h1>
+                {this.state.toggle? <p>hello world!</p>: <p>toggle switched off!</p>}
+                <button onClick={this.toggleVisibility}>Toggle</button>
+            </div>
+        )
+    }
 }
 
-render();
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById("app"));
+
+
+// let toggle = true;
+// const changeToggle = () => {
+//     toggle = !toggle
+//     console.log("toggle", toggle)
+//     render()
+// };
+
+// const app = document.getElementById("app")
+
+
+// const render = () => {
+//     const template = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={changeToggle} toggle={true}>{toggle ? 'Show Details' : 'Hide Details'}</button>
+//             <p hidden={toggle}>Hey, this is the magic text that gets hidden and appear everytime you click the button above.</p>
+//         </div>
+//     )
+
+//     ReactDOM.render(template, app)
+// }
+
+// render();
