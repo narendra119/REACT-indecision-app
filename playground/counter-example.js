b7569b8610b315b53bd4a40d508a9038d65e3f6f
pbabu01t <pbabu01t@knowledgenettest.onmicrosoft.com>
2024-12-22 09:21:15 +0530

    DefaultProps - Implementation on Counter

--- playground/counter-example.js
+++ playground/counter-example.js
@@ -7,7 +7,7 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: 0
+            count: props.count
         }
     }
 
@@ -47,6 +47,10 @@ class Counter extends React.Component {
     }
 }
 
+Counter.defaultProps = {
+    count: 0
+}
+
 ReactDOM.render(<Counter />, document.getElementById('app'))
 
 
@@ -74,7 +78,7 @@ ReactDOM.render(<Counter />, document.getElementById('app'))
 //             <button id="reset-button" onClick={resetCount}>Reset</button>
 //         </div>
 //     )
-        
+
 //     ReactDOM.render(templateThree, appRoot)
 // }
 
