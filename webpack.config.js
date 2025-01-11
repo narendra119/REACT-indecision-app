a87a1024a1f136d73779c0b734df8d68b0315135
pbabu01t <pbabu01t@knowledgenettest.onmicrosoft.com>
2025-01-11 09:23:11 +0530

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
 const path = require('path')
 
 module.exports = {
+    mode: 'development',
     entry: './src/app.js',
     output: {
         path : path.join(__dirname, 'public'),
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env', '@babel/preset-react'],
+                },
+            },
+        }],
+    },
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+        static: path.join(__dirname, 'public'),
+    },
 }
